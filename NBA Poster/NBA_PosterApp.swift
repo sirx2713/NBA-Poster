ae3c8b820356b7dfa4bdc8ffc1e792eb40b22c24
iSAD <bradx2713@gmail.com>
2024-08-22 15:33:15 +0200

    Initial Commit

---
+++ NBA Poster/NBA_PosterApp.swift
@@ -0,0 +1,17 @@
+//
+//  NBA_PosterApp.swift
+//  NBA Poster
+//
+//  Created by Tafadzwa Alexander Razaro on 2024/08/22.
+//
+
+import SwiftUI
+
+@main
+struct NBA_PosterApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
